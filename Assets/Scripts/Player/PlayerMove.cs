7775300301802f9cc5f6eb97f3e51d855b7024f5
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2025-08-18 00:26:48 +0900

    マウス操作

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,11 +4,25 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float speed = 1.0f;
 
+    public Transform leftWall;
+    public Transform rightWall;
+
     private void Update()
     {
-        float move = Input.GetAxis("Horizontal");
+        if (Input.GetMouseButton(0)) // 左クリック中だけ
+        {
+            Vector3 mouse = Input.mousePosition;
+            mouse.z = 10f; // カメラからの距離
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mouse);
 
-        Vector3 dir = new Vector3(move, 0, 0);
-        transform.Translate(dir * speed * Time.deltaTime);
+            // バーの半分の幅を考慮
+            float halfWidth = transform.localScale.x / 2f;
+            float minX = leftWall.position.x + halfWidth;
+            float maxX = rightWall.position.x - halfWidth;
+
+            float clampX = Mathf.Clamp(worldPos.x, minX, maxX);
+
+            transform.position = new Vector3(clampX, transform.position.y, transform.position.z);
+        }
     }
 }
