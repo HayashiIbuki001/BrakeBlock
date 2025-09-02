ca62268c992eaafe0462180dbcf3ad8d33adbc0d
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2025-09-02 12:30:32 +0900

    配置

---
+++ Assets/Scripts/System/BlockPlaceManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BlockPlaceManager : MonoBehaviour
+{
+    [SerializeField] public GameObject blockPrefub;
+    [SerializeField,Tooltip("行(縦)のブロック数")] private int rows;
+    [SerializeField, Tooltip("列(横)のブロック数")] private int cols;
+    [SerializeField] private float xSpace;
+    [SerializeField] private float ySpace;
+
+    private void Start()
+    {
+        Vector2 startPos = transform.position;
+        float OffsetRows = (rows - 1) * 0.5f * ySpace;
+        float OffsetCols = (cols - 1) * 0.5f * xSpace;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Vector2 pos = new Vector2(j * xSpace - OffsetRows, i * ySpace - OffsetCols) + startPos;
+                Instantiate(blockPrefub, pos, Quaternion.identity);
+            }
+        }
+    }
+}
